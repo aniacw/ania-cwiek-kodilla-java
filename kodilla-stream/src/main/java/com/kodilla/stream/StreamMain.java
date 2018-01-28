629e865059405adfc06c837aed9382f403bea3fc
aniacw <cwiek.ania@gmail.com>
2018-01-28 22:57:29 +0100

    7,3,2

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,6 +1,11 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.book.Book;
+import com.kodilla.stream.book.BookDirectory;
+import com.kodilla.stream.forumuser.Forum;
+import com.kodilla.stream.forumuser.ForumUser1;
+import com.kodilla.stream.immutable.ForumUser;
 import com.kodilla.stream.iterate.NumbersGenerator;
 import com.kodilla.stream.lambda.ExecuteSaySomething;
 import com.kodilla.stream.lambda.Executor;
@@ -8,6 +13,10 @@ import com.kodilla.stream.lambda.ExpressionExecutor;
 import com.kodilla.stream.lambda.Processor;
 import com.kodilla.stream.person.People;
 import com.kodilla.stream.reference.FunctionalCalculator;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class StreamMain {
     public static void main(String[] args) {
@@ -44,5 +53,55 @@ public class StreamMain {
                 .filter(s -> s.substring(0, 1).equals("M"))
                 .forEach(System.out::println);
 
-    }
+        BookDirectory theBookDirectory = new BookDirectory();
+        theBookDirectory.getList().stream()
+                .filter(book -> book.getYearOfPublication() > 2005)
+                .forEach(System.out::println);
+
+        System.out.println();
+
+        List<Book> theResultListOfBooks = theBookDirectory.getList().stream()
+                .filter(book -> book.getYearOfPublication() > 2005)
+                .collect(Collectors.toList());
+
+        System.out.println("# elements: " + theResultListOfBooks.size());
+        theResultListOfBooks.stream()
+                .forEach(System.out::println);
+
+        System.out.println();
+
+        Map<String, Book> theResultMapOfBooks = theBookDirectory.getList().stream()
+                .filter(book -> book.getYearOfPublication() > 2005)
+                .collect(Collectors.toMap(Book::getSignature, book -> book));
+
+        System.out.println("# elements: " + theResultMapOfBooks.size());
+        theResultMapOfBooks.entrySet().stream()
+                .map(entry -> entry.getKey() + ": " + entry.getValue())
+                .forEach(System.out::println);
+
+        System.out.println();
+
+        String theResultStringOfBooks = theBookDirectory.getList().stream()
+                .filter(book -> book.getYearOfPublication() > 2005)
+                .map(Book::toString)
+                .collect(Collectors.joining(",\n","<<",">>"));
+
+        System.out.println(theResultStringOfBooks);
+
+        System.out.println();
+        System.out.println("7.3");
+        System.out.println();
+
+        Forum myForum = new Forum();
+        Map<Integer, ForumUser> theResultListOfUsers = myForum.getUserList().stream()
+                .filter(forumUser1 -> forumUser1.getSex() == 'M')
+                .filter(forumUser1 -> forumUser1.localDate.getYear())
+                .filter(forumUser1 -> forumUser1.getPostPublished() >= 1)
+                .collect(Collectors.toMap(ForumUser1:: getUserId, forumUser1 -> forumUser1));
+
+        System.out.println("Number of elements: " + theResultListOfUsers.size());
+        theResultListOfUsers.entrySet().stream()
+                .map(entry -> entry.getKey() + ": " + entry.getValue())
+                .forEach(System.out:: println);
+        }
 }
--- kodilla-stream/src/main/java/com/kodilla/stream/forumuser/Forum.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/forumuser/Forum.java
@@ -1,4 +1,25 @@
 package com.kodilla.stream.forumuser;
 
-public class Forum {
+import java.util.ArrayList;
+import java.util.List;
+
+public final class Forum {
+
+    private final List<ForumUser1> userList = new ArrayList<>();
+
+    public Forum(){///????
+        userList.add(new ForumUser1(0445, "Thomas99", 'M', 2000, 04, 28, 16));
+        userList.add(new ForumUser1(1422, "Michael", 'M', 1977, 12, 02, 0));
+        userList.add(new ForumUser1(1233, "Jessica5454", 'W', 1998,12, 28, 0));
+        userList.add(new ForumUser1(0775, "Shaft", 'M', 1995, 02, 16, 528));
+        userList.add(new ForumUser1(1544, "Ginger", 'W', 1992, 07, 04, 885));
+        userList.add(new ForumUser1(0177, "Minessota2",'W', 1999,10, 19, 2514));
+        userList.add(new ForumUser1(1452, "Nosferatu", 'M', 2001, 01, 31, 821));
+        userList.add(new ForumUser1(0326, "NoUserName", 'M', 2005, 11,18, 75));
+        userList.add(new ForumUser1(1439, "Madagaskar", 'W', 2003, 05, 17, 4));
+    }
+
+    public List<ForumUser1> getUserList() {
+        return userList;
+    }
 }
