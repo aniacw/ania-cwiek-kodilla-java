895332594db15fbd50800337ea63fde47dd21aaf
aniacw <cwiek.ania@gmail.com>
2018-02-08 17:21:40 +0100

    8.3.2

--- kodilla-exception/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
@@ -1,16 +1,15 @@
 package com.kodilla.exception.test;
 
 public class ExceptionHandling extends Exception {
-//    public static void main(String[] args) {
-//        SecondChallenge secondChallenge = new SecondChallenge();
-//
-//        try {
-//            secondChallenge.probablyIWillThrowException();
-//        } catch (SecondChallenge e){
-//            System.out.println("Error");
-//        }finally {
-//            System.out.println("End");
-//        }
-//
-//    }
+    public static void main(String[] args) {
+        SecondChallenge secondChallenge = new SecondChallenge();
+
+        try {
+            secondChallenge.probablyIWillThrowException(7.8, 4.7);
+        } catch (Exception e){
+            System.out.println("Error");
+        }finally {
+            System.out.println("End");
+        }
+    }
 }
